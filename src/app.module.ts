54fc0335705e03e92b263ba1bab4ef4a1c43eb58
Thalia Cavalcanti <thaliascavalcanti@gmail.com>
2024-07-10 21:16:32 -0300

    atividade do banco

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,9 @@ import { Module } from '@nestjs/common';
 import { BancoController } from './controllers/banco.controller';
 import { BancoService } from './services/banco.service';
 
-
-
 @Module({
   imports: [],
   controllers: [BancoController],
   providers: [BancoService],
 })
-export class AppModule { }
+export class AppModule {}
--- src/models/cliente.model.ts
+++ src/models/cliente.model.ts
@@ -1,33 +1,32 @@
-import { TipoContaBancaria } from "src/enums/tipo-conta-bancaria.enum";
-import { ContaBancaria } from "./conta-bancaria.model";
-import { randomUUID } from "node:crypto";
-import { Gerente } from "./gerente.model";
+import { TipoContaBancaria } from 'src/enums/tipo-conta-bancaria.enum';
+import { ContaBancaria } from './conta-bancaria.model';
+import { randomUUID } from 'node:crypto';
+import { Gerente } from './gerente.model';
 
 export class Cliente {
-    
-    constructor(
-        public nomeCompleto: string,
-        public endereco: string,
-        public id: string,
-        public telefone: string,
-        public gerente: Gerente | null = null,
-        public contas: ContaBancaria[] = []
-    ) {
-        this.id = randomUUID()
-    }
+  constructor(
+    public nomeCompleto: string,
+    public endereco: string,
+    public id: string,
+    public telefone: string,
+    public gerente: Gerente | null = null,
+    public contas: ContaBancaria[] = [],
+  ) {
+    this.id = randomUUID();
+  }
 
-    abrirConta(conta: ContaBancaria): void {
-        this.contas.push(conta);
-    }
+  abrirConta(conta: ContaBancaria): void {
+    this.contas.push(conta);
+  }
 
-    fecharConta(conta: ContaBancaria): void {
-        const index = this.contas.indexOf(conta);
-        if (index !== -1) {
-            this.contas.splice(index, 1);
-        }
+  fecharConta(conta: ContaBancaria): void {
+    const index = this.contas.indexOf(conta);
+    if (index !== -1) {
+      this.contas.splice(index, 1);
     }
+  }
 
-    mudarTipoConta(conta: ContaBancaria, novoTipo: TipoContaBancaria): void {
-        conta.tipo = novoTipo;
-    }
+  mudarTipoConta(conta: ContaBancaria, novoTipo: TipoContaBancaria): void {
+    conta.tipo = novoTipo;
+  }
 }
